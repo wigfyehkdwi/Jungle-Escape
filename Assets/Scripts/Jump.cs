e099f928eae3fcb4d248db5422aa6648ef24f552
madeleoh <maddieeleonore2009@gmail.com>
2024-02-24 11:57:53 -0500

    last commit

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    float jumpForce = 5.7f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump")){
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
+
