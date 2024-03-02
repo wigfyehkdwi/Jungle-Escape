cf34877a7e21efbf0cfb1c6f28a4a703d4f616a9
madeleoh <maddieeleonore2009@gmail.com>
2024-03-02 12:54:43 -0500

    erg3qhja434253t4g35htyt6yt6235trh534yt35trh54y3t5tryfhgmtfryjn654utyuytkrj564ujtyr564uytu564y5y46ytr564y554ythjmuyj5645t453t4ytt4543edfvb nhy653wsxxxxxxxxxxxxxcvbnhyt5432qasdfgty652wetgy6t5

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -7,6 +7,8 @@ public class Jump : MonoBehaviour
     Rigidbody rigidbody;
 
     float jumpForce = 5.7f;
+    float fallMultiplier = 1.5f;
+    bool isGrounded = false;
 
     void Start()
     {
@@ -15,8 +17,17 @@ public class Jump : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetButtonDown("Jump")){
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, .15f);
+        Debug.DrawRay(transform.position, Vector3.down * .15f, Color.red);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
             rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if (rigidbody.velocity.y < 0)
+        {
+            rigidbody.velocity += Physics.gravity * fallMultiplier * Time.deltaTime;
         }
     }
 }
